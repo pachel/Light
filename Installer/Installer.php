458f4ad1f407182df4ea328a851a63e182cf23eb
pachel82,user.email <32448562+pachel@users.noreply.github.com>
2025-10-23 10:57:01 +0200

    Simítások

---
+++ Installer/Installer.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pachel\Light\Install;
+
+class Installer
+{
+
+    private static $root;
+    public static function postInstall()
+    {
+        self::$root = __DIR__."/../../../";
+        copy(__DIR__."/.htaccess",self::$root.".htaccess");
+
+    }
+
+    public static function postUpdate()
+    {
+
+    }
+}
